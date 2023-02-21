d891efa4f0330c1e305a9c838c7739d7672c90e9
uminocelo <marcelo.mdl.dev@gmail.com>
2023-02-21 20:49:40 -0300

    chore:  create project \o/

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Overpass_900Black } from '@expo-google-fonts/overpass';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Overpass_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Overpass_900Black', fontSize: 40 }}>Olá mundo com Overpass Black</Text>
       <StatusBar style="auto" />
     </View>
   );
